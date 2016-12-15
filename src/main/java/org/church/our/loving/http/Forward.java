4a4d10df3f5fcd11103cfb0704f7a0b024860152
Nicky Jay <sunxiaf22@126.com>
2016-12-15 09:44:55 +0800

    Add files via upload

--- src/main/java/org/church/our/loving/http/Forward.java
+++ src/main/java/org/church/our/loving/http/Forward.java
@@ -2,12 +2,15 @@ package org.church.our.loving.http;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.church.our.loving.constants.IOurChurchConstants;
 import org.church.our.loving.util.StringUtil;
 
@@ -16,6 +19,7 @@ import org.church.our.loving.util.StringUtil;
  */
 public class Forward extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static Logger logger = Logger.getLogger("Forward");
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,14 +32,22 @@ public class Forward extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		//Update Domain
 		String type = request.getParameter("newdomain");
-		String app = request.getParameter("app");
 		if (!StringUtil.isEmpty(type)) {
 			FileUtils.write(new File(IOurChurchConstants.NEW_DOMAIN_STORE), type, "utf-8");
-		} else {
+			out.println("<P> Domain uploaded successfully! <p>");
+			String existingDomain = FileUtils.readFileToString(new File(IOurChurchConstants.NEW_DOMAIN_STORE), "utf-8");
+			out.println("<p> New domain is : " + existingDomain + "</p>");
+			logger.debug("New domain is : " + existingDomain);
+		}
+		//Forward links
+		else {
+			String app = request.getParameter("app");
 			String existingDomain = FileUtils.readFileToString(new File(IOurChurchConstants.NEW_DOMAIN_STORE), "utf-8");
 			if (!StringUtil.isEmpty(existingDomain)) {
-				if (StringUtil.isEmpty(app)) {
+				if (!StringUtil.isEmpty(app)) {
 					response.sendRedirect(existingDomain + "/" + app);
 				} else {
 					response.sendRedirect(existingDomain + "/app2");
